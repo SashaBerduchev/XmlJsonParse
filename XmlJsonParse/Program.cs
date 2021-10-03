30bb468de5458df3fe463de35fe5ea05746f0406
Санёк Виничук <sashavinichuk@gmail.com>
2021-10-03 13:46:17 +0300

    First

---
+++ XmlJsonParse/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XmlJsonParse
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets start");
+            OpenXml();
+            
+        }
+
+        public static void OpenXml()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                try
+                {
+                    
+                    string jsonstring = "";
+                    openFileDialog.ShowDialog();
+                    var file = openFileDialog.FileName.ToString();
+                    XmlDocument xmlDocument = new XmlDocument();
+                    xmlDocument.Load(file);
+                    jsonstring = JsonConvert.SerializeXmlNode(xmlDocument, Newtonsoft.Json.Formatting.Indented);
+                    Console.WriteLine(jsonstring);
+                    Console.Read();
+                }
+                catch(Exception exp)
+                {
+                    Console.WriteLine(exp.ToString());
+                    Console.Read();
+                }
+                
+            }
+        }
+    }
+}
